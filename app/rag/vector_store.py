e7633c85651f24f322b85b998f066c659c1387e1
UjjwalKarkeyy <ujjwalkarki.official@gmail.com>
2025-11-20 00:09:32 -0800

    initialized qdrant client

--- app/rag/vector_store.py
+++ app/rag/vector_store.py
@@ -1,8 +1,8 @@
 from qdrant_client import QdrantClient
-from app.config import QDRANT_API
+from app.config import QDRANT_API, QDRANT_HOST
 
 # Initialize Qdrant client
 client = QdrantClient(
-    host=QDRANT_HOST,
-    api_key=QDRANT_API_KEY,
+    host= QDRANT_HOST,
+    api_key= QDRANT_API,
 )
